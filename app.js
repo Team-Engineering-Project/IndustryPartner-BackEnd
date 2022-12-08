16f569a873fd0852998cf356a937e9554a0ae471
Rafee Ahmed <raffe.ahmed@hotmail.co.uk>
2022-12-08 10:33:41 +0000

    editted .env and app (attempt to resolve heroku deployment issue

--- app.js
+++ app.js
@@ -9,22 +9,10 @@ dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
 import { router as graduatesRoute } from './routes/graduatesRoute.js';
 import authRoutes from './routes/authRoutes.js';
 
-const port = process.env.PORT;
-const host = process.env.HOST;
+const PORT = process.env.PORT;
 const app = express();
 
-const main = async () => {
-    console.log(`Connecting to DB @ ${process.env.DB_URI}`);
-    await mongoose.connect(process.env.DB_URI);
-}
-
-main().catch(err => console.log(err));
-
-app.use(cors({
-    origin: ["http://localhost:3000"],
-    methods: ["GET", "POST"],
-    credentials: true,
-  }));
+app.use(cors());
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(express.json());
@@ -32,10 +20,8 @@ app.use(express.json());
 app.use("/", authRoutes);
 app.use(`/graduates`, graduatesRoute);
 
-const server = app.listen(port, host, () => {
-    const SERVERHOST = server.address().address;
-    const SERVERPORT = server.address().port;
-    console.log(`Server is running on http://${SERVERHOST}:${SERVERPORT}`);
-})
+mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
 
-export default server;
+// mongoose.set('useFindAndModify', false);
