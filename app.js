cf239ec5d98f92a8c1e9160675eb46148b8c5465
Rafee Ahmed <raffe.ahmed@hotmail.co.uk>
2022-12-08 23:30:37 +0000

    make it better

--- app.js
+++ app.js
@@ -5,23 +5,28 @@ import cors from 'cors';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
-
 import { router as graduatesRoute } from './routes/graduatesRoute.js';
 import authRoutes from './routes/authRoutes.js';
 
-const PORT = process.env.PORT;
 const app = express();
 
-app.use(cors());
+
 app.use(bodyParser.json());
 app.use(cookieParser());
-app.use(express.json());
+app.use(express.json({ limit: '30mb', extended: true }));
+app.use(express.urlencoded({ limit: '30mb', extended: true }));
+app.use(cors());
 
 app.use("/", authRoutes);
 app.use(`/graduates`, graduatesRoute);
 
-mongoose.connect('mongodb+srv://DFXtraAdmin:j9kEjs9iB5VFyMOG@dfxtra.fiey4ml.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
-  .catch((error) => console.log(`${error} did not connect`));
+const PORT = process.env.PORT;
+const DB = process.env.DB_URI;
 
-// mongoose.set('useFindAndModify', false);
+const main = async () => {
+    await mongoose.connect(DB, { useNewUrlParser: true, useUnifiedTopology: true })
+}
+
+main()
+    .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+    .catch((error) => console.log(`${error} did not connect`));
